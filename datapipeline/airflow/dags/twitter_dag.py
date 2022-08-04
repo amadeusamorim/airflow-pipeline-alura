9101a52ec86a17708b8a52ba31bb1593c615ecff
amadeusamorim <amorim.amadeus@gmail.com>
2022-08-04 10:00:38 -0300

    Integrando Spark ao Airflow

--- datapipeline/airflow/dags/twitter_dag.py
+++ datapipeline/airflow/dags/twitter_dag.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from os.path import join
+from pathlib import Path
+
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.models import DAG
 from airflow.operators.amadeus import TwitterOperator
 
@@ -13,4 +16,18 @@ with DAG(dag_id="twitter_dag", start_date=datetime.now()) as dag:
                 "extract_date={{ ds }}", # partição com a data de extraçao
                 "FlamengoMalvadao_{{ ds_nodash }}.json"
                 )
+    )
+
+    twitter_transform = SparkSubmitOperator(
+        task_id = "transform_twitter_flamengomalvadao",
+        application="/home/amadeus/ama/airflow-pipeline-alura/datapipeline/spark/transfortmation.py",
+        name="twitter_transformation", # Nome spark chama o job
+        application_args = [
+            "--src",
+            "/home/amadeus/ama/airflow-pipeline-alura/datapipeline/datalake/bronze/twitter_flamengomalvadao/extract_date=2022-08-02",
+            "--dest",
+            "/home/amadeus/ama/airflow-pipeline-alura/datapipeline/datalake/silver/twitter_flamengomalvadao",
+            "--process-date",
+            "{{ ds }}"
+        ]
     )
